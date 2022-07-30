f6c001677c62b7a1015246c1df748eee3757df85
Your Name <you@example.com>
2022-07-30 23:20:53 +0530

    [Megha] Add: Playing Position At Zero

---
+++ SnakeAndLadderGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeAndLadder
+{
+    //UC1
+    public static class SnakeAndLadderGame
+    {
+        const int position = 0;
+        public static void CheckDieRoll()
+        {
+            Console.WriteLine("The starting position is -> "+position);
+        }
+    }
+}
