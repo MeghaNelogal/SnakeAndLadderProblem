4a522493eb4c7a1bc7fe829dec52afbe497853be
Your Name <you@example.com>
2022-07-30 23:21:44 +0530

    [Megha] Add: UC1 Added

--- Program.cs
+++ Program.cs
@@ -4,7 +4,8 @@
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("***Welome To Snake And Ladder Problem***");
+            Console.WriteLine("***Welome To Snake And Ladder Problem***\n");
+            SnakeAndLadderGame.CheckDieRoll();
         }
     }
 }
