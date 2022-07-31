3fc46bedff8b3361e76300f25a31e056cbb8cf3e
Your Name <you@example.com>
2022-07-31 09:40:58 +0530

    [Megha] Add: UC7 Added

--- Program.cs
+++ Program.cs
@@ -6,8 +6,61 @@
         {
             Console.WriteLine("***Welome To Snake And Ladder Problem***\n");
             SnakeAndLadderGame snakeAndLadderGame = new SnakeAndLadderGame();
-            snakeAndLadderGame.CheckDieRoll();
-            snakeAndLadderGame.Game();
+            snakeAndLadderGame.start();
+        }
+        public class Player
+        {
+            public int position = 0;
+            public static Random random = new Random();
+            public int roll;
+            public int totalRoll;
+            public void Move(int movement)
+            {
+                position += movement;
+                if (position > 100)
+                {
+                    position -= movement;
+                    Console.WriteLine("You crossed 100 so moving back to last position");
+                }
+            }
+            public void RollDice()
+            {
+                roll = random.Next(1, 7);
+                totalRoll++;
+                Display();
+                Console.WriteLine("Player rolled: " + roll);
+                Move(roll);
+                Option();
+            }
+            public void Display()
+            {
+                Console.WriteLine("Player position: " + position);
+            }
+            public void Option()
+            {
+                int option = random.Next(0, 3);
+                switch (option)
+                {
+                    case 0:
+                        Console.WriteLine("No Play");
+                        break;
+
+                    case 1:
+                        Console.WriteLine("You are on Snake");
+                        Move(-roll);
+                        break;
+
+                    case 2:
+                        Console.WriteLine("You found a ladder");
+                        Move(roll);
+                        break;
+
+                    default:
+                        Console.WriteLine("Error");
+                        break;
+                }
+            }
         }
     }
 }
+    
